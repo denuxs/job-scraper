a519803bfd7b86f14f0a3737c444b1e4bdffadba
Denis Lopez <denuxs@gmail.com>
2024-10-29 11:48:40 -0600

    init project

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.subheader("Empleos Nicaragua")
+st.write(
+    "Aplicación Web que extrae ofertas laborales en Nicaragua de los sitios web: encuentra24, computrabajo y tecoloco"
+)
+st.write(
+    "Desarrollado con fines educativos con Python, Beautifulsoup y Streamlit, la carga puedo tomar algunos segundos para evitar Baneo y esta alojado en la platadorma Render"
+)
